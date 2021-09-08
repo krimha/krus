ada67794cf45bc21a4522cedf6dbaccac4ee8865
Kristian Monsen Haug <kristian.haug95@gmail.com>
2021-09-08 21:44:12 +0200

    Implement match function for Finite State automaton

--- automaton.go
+++ automaton.go
@@ -2,9 +2,54 @@ package krus
 
 type node struct {
 	name string
-	edges map[byte]*node
+	edges map[rune]*node
 }
 
 func NewNode(name string) *node {
-	return &node { name, make(map[byte]*node) }
+	return &node { name, make(map[rune]*node) }
+}
+
+// GRAPH
+
+type Graph struct {
+	nodes map[string]*node
+	start *node
+	accept []*node
+}
+
+func NewGraph(nodeNames []string, start string, acceptStates []string) Graph {
+	graph := Graph{ make(map[string]*node), nil, []*node{}}
+
+	for _, name := range nodeNames {
+		graph.nodes[name] = NewNode(name)
+	}
+
+	graph.start = graph.nodes[start]
+
+	for _, name := range acceptStates {
+		graph.accept = append(graph.accept, graph.nodes[name])
+	}
+
+	return graph
+}
+
+func (graph *Graph) Connect(source string, target string, symbol rune) {
+	sourceNode := graph.nodes[source]
+	targetNode := graph.nodes[target]
+	sourceNode.edges[symbol] = targetNode
+}
+
+func (graph Graph) Match(tomatch string) bool {
+	currentNode := graph.start
+	for _, symbol := range tomatch {
+		currentNode = currentNode.edges[symbol]
+	}
+
+	// Check if accept state
+	for _, node := range graph.accept {
+		if node == currentNode {
+			return true
+		}
+	}
+	return false
 }
--- automaton_test.go
+++ automaton_test.go
@@ -10,3 +10,35 @@ func TestNewNode(t *testing.T) {
 		t.Fatalf(`NewNode("q1").name = %s, exptected %s`, result, expected)
 	}
 }
+
+func TestNewGraph(t *testing.T) {
+	g := NewGraph([]string{"q0", "q1"}, "q0", []string{"q1"})
+
+	if g.nodes["q0"] == nil {
+		t.Fatalf("Node q0 was not initializeed")
+	}
+
+	if g.nodes["q0"].name != "q0" {
+		t.Fatalf(`Node q0 was given wrong name "%s"`, g.nodes["q0"].name)
+	}
+
+	if g.start != g.nodes["q0"] {
+		t.Fatalf("Start node was not set correctly")
+	}
+}
+
+func TestMatch(t *testing.T) {
+	g := NewGraph([]string{"q0", "q1"}, "q0", []string{"q1"})
+	g.Connect("q0", "q1", '1')
+	g.Connect("q1", "q0", '0')
+	g.Connect("q0", "q0", '0')
+	g.Connect("q1", "q1", '1')
+
+	if g.Match("00000") {
+		t.Fatalf(`Should not match "00000"`)
+	}
+	if !g.Match("000111") {
+		t.Fatalf(`Should match "000111"`)
+	}
+	
+}
